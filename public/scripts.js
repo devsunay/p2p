705ec14eb44176274aef9c1366ef89f6ad91e612
sunay kundalwal <sunaykundalwal@outlook.in>
2026-02-25 19:59:52 +0530

    second commit

--- public/scripts.js
+++ public/scripts.js
@@ -5,7 +5,7 @@ const password = "x";
 
 document.querySelector("#user-name").innerHTML = userName;
 
-const socket = io.connect('https://192.168.85.7:8181',
+const socket = io.connect('https://p2p-production-a9fb.up.railway.app:8080',
     {
         auth:{
         userName,
